2d617f041f3516076f2da08b2e788d6c75e9aa92
dilshodturabov <avlomberdioglid@gmail.com>
2022-09-10 18:05:04 -0700

    added loader

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const active = document.querySelector(".active");
 const sections = document.querySelectorAll(".sections");
 const nav = document.querySelector(".nav");
-
+const preloader = document.querySelector(".preloader");
+window.addEventListener("load", function() {
+    preloader.classList.add("hide-preloader");
+})
 sections.forEach( function(link) {
     link.addEventListener("click", ()=>{
         nav.style.transform = translate(10, 0)
